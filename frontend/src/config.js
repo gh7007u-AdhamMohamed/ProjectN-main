49210717f50972fd89b802f618bd966dabe5ecb4
gh7007u-AdhamMohamed <gh7007u@gmail.com>
2026-04-18 09:59:15 +0200

    Update BASE_URL to new project backend URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,3 @@
-const BASE_URL = 'https://your-backend-name.vercel.app'
+const BASE_URL = "https://project-n-main-n48t-dcu3lncye-gh7007u-adhammohameds-projects.vercel.app"
+
 export default BASE_URL
